5961af628aa3669c8a36bb0f6ee1e69f5a2f95d2
tanmaylohakare <tanmaylohakare820@gmail.com>
2024-11-27 15:50:53 +0530

    Single Player

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    private static final int START_POSITION = 0;
+
+
     public static void main(String[] args) {
 
         System.out.println("WelCome Snake And Ladder");
 
-        
+        System.out.println("Welcome to Snake And Ladder Game\n");
+        int playerPosition = START_POSITION;
+        System.out.println("Player starts at position: " + playerPosition);
 
-
-    }
+}
 }
